3816784b466357f483e55928fca6491baad83e9c
FelipeF-soares <felipe.fends@gmail.com>
2025-01-07 16:10:50 -0300

    Adicionar arquivos de projeto.

---
+++ BedBookingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BedBookingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
